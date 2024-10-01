f7722eb15e7225bc0f819412877ead9261672b88
SergioLebron22 <lebronsergio22@gmail.com>
2024-10-01 16:50:19 -0400

    feat: added login/logout logic and refactored user model
    - Implemented login and logout functionality
    - Refactored CustomUser model to better align with Django's authentication system

--- kame/kame_app/admin.py
+++ kame/kame_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'name', 'is_staff', 'is_superuser']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
